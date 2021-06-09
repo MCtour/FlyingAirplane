cb37bc5421070e01454b70f967907306b35535fb
akumar <aikamkaushal@gmail.com>
2021-06-09 14:38:44 -0500

    add invisible wall as cillison target for plane to reset to its initial position

---
+++ Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    Vector3 originalPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("AEROPLANEWALL"))
+        {
+            transform.localPosition = originalPosition;
+        }
+    }
+}
